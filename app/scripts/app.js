8d45f651ddcdfeecad6ce9fe94c23d0b1b6de899
Andrew Smith <andrew@asmithdigital.com>
2016-11-24 09:50:50 +1030

    playing with saving data to firebase
    Signed-off-by: Andrew Smith <andrew@asmithdigital.com>

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,16 @@
 var app = angular.module("sampleApp", ["firebase"]);
-app.controller("SampleCtrl", function($scope, $firebaseObject) {
-  var ref = firebase.database().ref().child("data");
-  // download the data into a local object
-  var syncObject = $firebaseObject(ref);
-  // synchronize the object with a three-way data binding
-  // click on `index.html` above to see it used in the DOM!
-  syncObject.$bindTo($scope, "data");
+
+app.controller("SampleCtrl", function($scope, $firebaseArray) {
+
+  var ref = firebase.database().ref().child("messages");
+  // create a synchronized array
+  $scope.messages = $firebaseArray(ref);
+  // add new items to the array
+  // the message is automatically added to our Firebase database!
+  $scope.addMessage = function() {
+    $scope.messages.$add({
+      text: $scope.newMessageText
+    });
+  };
+
 });
