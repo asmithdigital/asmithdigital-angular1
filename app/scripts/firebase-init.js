bd2457d8781fc5b6e7727768e50444f929704d1a
Andrew Smith <andrew@asmithdigital.com>
2016-11-24 14:45:50 +1030

    move to angular material, remove bootstrap, split firebase scripts
    Signed-off-by: Andrew Smith <andrew@asmithdigital.com>

---
+++ app/scripts/firebase-init.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyB09cIBjkTTj1eY-xBnS-nkKKjfMzNHPbs",
+  authDomain: "asmithdigital-67949.firebaseapp.com",
+  databaseURL: "https://asmithdigital-67949.firebaseio.com",
+  storageBucket: "asmithdigital-67949.appspot.com",
+  messagingSenderId: "641452325318"
+};
+firebase.initializeApp(config);
